c66fe17be2b31acba3eac4bcd5534ec68b0679ff
Luis Oballos Mancini <luis.oballosm@gmail.com>
2025-02-19 18:55:31 +0000

    First commit

---
+++ add.js
@@ -0,0 +1,64 @@
+/* 
+Funciones a elaborar:
+
+* animateByLine: 
+    Esta función debe imprimir el corazón línea por línea en la consola del editor, debe de haber un salto de línea entre cada impresión. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de línea.
+
+* animateByChart: 
+    Esta función debe imprimir el corazón carácter por carácter en la consola del editor, debe de haber un delay entre cada impresión de carácter. 
+    Cada vez que se complete la impresión de una línea, se debe hacer un salto de línea. 
+    Recibe un parámetro llamado "ms" que indica el retraso en milisegundos entre cada impresión de carácter.
+
+Pistas:
+
+- Investiga sobre la función setTimeout, puede ser de interés.
+- Usa console.log o process.stdout.write para imprimir en la consola de acuerdo a lo que necesites.
+- Recuerda que necesitas iterar un string, ¿cómo lo conviertes en un array de caracteres?
+
+Para iniciar el proyecto, ejecuta node app.js
+
+*/
+
+
+
+
+const asciiHeart = [
+    "⣠⣤⣶⣶⣦⣄⡀  ⠀⢀⣤⣴⣶⣶⣤⣀",
+    "⣼⣿⣿⣿⣿⣿⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣧",
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏",
+    " ⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋ ",
+    "   ⠙⢿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀",
+    "     ⠉⢿⣿⣿⣿⠟⠋⠀",
+    "⠀      ⠙⠻⠁"
+]
+
+const animateByLine = ms => {
+    asciiHeart.forEach((line, index) => {
+        setTimeout(() => {
+            console.log(line)
+        }, ms * index)
+    })
+}
+
+const animateByChart = ms => {
+    let delay = 0
+    asciiHeart.forEach(line => {
+        line.split('').forEach((char, charIndex) => {
+            delay += ms * charIndex
+            setTimeout(() => {
+                if (charIndex === line.length - 1) {
+                    process.stdout.write(char)
+                    process.stdout.write('\n')
+                }
+                else {
+                    process.stdout.write(char)
+                }
+            }, delay)
+        })
+    })
+}
+
+animateByChart(5);
+//animateByLine(100)
